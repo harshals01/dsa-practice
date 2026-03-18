f784c0b23d6f2ef06261b3b431d5290e3738cf49
Harshal <146689301+harshals01@users.noreply.github.com>
2026-03-18 23:09:34 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+
+    int a,b,c;
+    a=10;
+    b=8;
+    c=5;
+    int sum = a + (b-c)/2;
+
+cout << sum << endl;
+    return 0;
+
+
+}
+
+
