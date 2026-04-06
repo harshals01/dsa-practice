68025ac63f58e7802ea7d6c85ef2d9aeff2a67d8
Harshal <146689301+harshals01@users.noreply.github.com>
2026-04-06 23:55:07 +0530

    Add vector test with size and capacity output
    Initialize a vector and print its size and capacity.

---
+++ vectorstest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+
+    vector<int> v;
+    cout << "size: " << v.size() << endl;
+    cout << "size: " << v.capacity();
+
+    v.push_back(1);
+    cout << 
+
+    return 0;
+}
