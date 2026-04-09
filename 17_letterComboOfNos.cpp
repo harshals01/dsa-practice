a12dff46dd59452e4dbb354937cd1c71d07f0372
Harshal <harshals008@gmail.com>
2026-04-09 22:27:23 +0530

    solve function

--- 17_letterComboOfNos.cpp
+++ 17_letterComboOfNos.cpp
@@ -12,7 +12,16 @@ void solve(string digit, string output, int index, vector<string> &ans, string m
     
     int number = digit[index] - '0';
     string value = mapping[number];
-}
+
+    for (int i = 0; i < value.length(); i++)
+    {
+        output.push_back(value[i]);
+        solve(digit, output, index+1, ans, mapping);
+        output.pop_back();
+    }
+    
+    
+} 
 
 int main(){ 
 
