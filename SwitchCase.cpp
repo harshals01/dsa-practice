7e398ee06bea92f0a9c56d766fd214002405d678
Harshal <harshals008@gmail.com>
2026-04-11 15:48:47 +0530

    switchcase statement usecase

---
+++ SwitchCase.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    // int num = 2;
+    char ch = '3';
+    cout << endl;
+
+
+    switch (ch)
+    {
+    case '3':
+        cout << "one" << endl;
+        break;
+
+        case 2:
+        cout << "two" << endl;
+        break;
+    
+    default:
+        cout << "none" << endl;
+
+        break;
+    }
+
+
+    return 0;
+
+
+}
+
+
