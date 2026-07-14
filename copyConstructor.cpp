b269fa1141772396e0ab48a5929c61aba48ecab8
Harshal <harshals008@gmail.com>
2026-07-15 01:01:21 +0530

    oops

---
+++ copyConstructor.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class Hero
+{
+private:
+    int age;
+
+public:
+
+    char *name;
+    char level;
+
+    Hero()
+    {
+        cout << "constructor called" << endl;
+        name = new char[100];
+    }
+
+    // parameterized constructor
+
+    Hero(int health, char level)
+    {
+
+        cout << "param constructor called" << endl;
+        this->health = health;
+        this->level = level;
+    }
+
+    // copy constructor
+
+    Hero(Hero& temp)
+    {
+
+        char *ch = new char[strlen(temp.name) + 1];
+        strcpy(ch , temp.name);
+        this -> name = ch;
+
+        cout << "copy constructor called" << endl;
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+
+    void print()
+    {
+        cout << "name is: " << this->name << endl;
+        cout << "health is: " << this->health << endl;
+        cout << "level is: " << this->level << endl;
+    }
+
+    int health;
+    // char level;
+
+    int getHealth(){ 
+        return health;}
+
+    void setHealth(int health){ 
+        this-> health = health;}
+
+    int getAge() { return age; }
+    void setAge(int a) { age = a; }
+};
+
+
+int main()
+{
+    Hero hero1;
+    hero1.setAge(20);
+    hero1.setHealth(80);
+    hero1.level = 'A';
+    strcpy(hero1.name, "mohan");
+    hero1.print();
+
+
+    Hero hero2(hero1);
+    hero2.print();
+
+    return 0;
+}
