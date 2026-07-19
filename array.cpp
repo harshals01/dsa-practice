e9a3add3a52c63102adec5e5d6bdfa6a13f36d8b
Harshal <harshals008@gmail.com>
2026-07-19 15:11:00 +0530

    file upload

--- array.cpp
+++ array.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
+// void printArray(int arr[], int size)
+// {
 
-void printArray(int arr[], int size){
+//     cout << "array is: " << endl;
 
-    cout << "array is: " << endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << " " << arr[i];
+//     }
+// }
+
+int getMax(int arr[], int size)
+{
+
+    int max1 = INT_MIN;
 
     for (int i = 0; i < size; i++)
     {
-        cout << " " << arr[i]  ;
+        max1 = max( max1, arr[i]);
     }
-
-    
+    return max1;
 }
 
-int main(){
+int getMin(int arr[], int size)
+{
 
-    int one[15] = {2,5};
-    printArray(one, 15);
-    cout << endl ;
+    int min1 = INT_MAX;
 
+    for (int i = 0; i < size; i++)
+    {
+      min1 = min(min1, arr[i]);
+    }
+    return  min1;
 
-    int oneSize = sizeof(one)/ sizeof(int);
+}
 
-    cout << "size of one is: " << oneSize;
-    return 0;
+int main()
+{
+
+    int size;
+    cin >> size;
+
+    int arr[100];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    cout << "size of arr is: " << size << endl;
+    cout << "max element in arr is: " << getMax(arr, size) << endl;
+    cout << "min element in arr is: " << getMin(arr, size);
 }
