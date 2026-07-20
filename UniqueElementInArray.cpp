563448d38edb77f361a185d7e8767433ec23979b
Harshal <harshals008@gmail.com>
2026-07-20 17:15:40 +0530

    unique element in an array

---
+++ UniqueElementInArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+bool isUnique(int arr[], int size, int n){
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == n){
+
+            int count = 0;
+    
+    for (int i = 0; i < size; i++)
+    {
+          
+        if (n == arr[i])
+        {
+            count++;
+        }
+
+        if (count >=2 )
+        {
+            cout << "element is not unique " << endl;
+            return 0;
+        }       
+    }
+    cout << "element is unique " << endl;
+    return 1;
+
+        }
+    }
+}
+
+int main(){
+
+
+    int one[5] = {2,5,3,2,9};
+    cout << "input element: " <<endl;
+    int n;
+    cin >> n;
+    isUnique(one, 5, n);
+
+    
+    return 0;
+}
