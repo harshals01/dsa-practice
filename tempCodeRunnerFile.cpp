a20927d16d0edc9a947cb9b83fadc4bdf40c388c
Harshal <harshals008@gmail.com>
2026-07-31 23:56:23 +0530

    file upload

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-    // char level;
+    // cout << "Student Name : " << obj1.name << endl;
+    // cout << "Student Rollno : " << obj1.height << endl;
+    // cout << "Student Age : " << obj1.getAge() << endl;
+    // cout << "Student weight : " << obj1.weight << endl;
