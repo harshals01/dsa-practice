bd994305acddbca599c87ffc5f8ffda79b71ce4f
Harshal <harshals008@gmail.com>
2026-08-07 22:45:31 +0530

    file upload

---
+++ polymorphism.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class landVehicle{
+
+    public:
+
+    string vehicleType;
+
+    public:
+
+    void landInfo(){
+        
+        cout << "works on land only" <<endl;
+
+    }
+
+    void landInfo(string vehicleType){
+
+        cout << "land vehc number is not defined" << endl;
+    }
+};
+
+
+int main(){
+
+    landVehicle car;
+    car.landInfo("car");
+   
+    return 0;
+}
