7bf5e4b1226cfc36d46e2b1ff5bc046bb01513cf
Harshal <harshals008@gmail.com>
2026-08-07 09:34:35 +0530

    file upload

---
+++ inheritanceAmbiguity.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class landVehicle{
+
+    public:
+
+    int chassis;
+
+    public:
+
+    void landInfo(){
+        
+        cout << "works on land only" <<endl;
+
+    }
+
+    void vehicleNo(){
+
+        cout << "land vehc number is not defined" << endl;
+    }
+};
+
+class waterVehicle{
+
+    public:
+
+    void waterInfo(){
+
+                cout << "works in water only" <<endl;
+
+    }
+
+    void vehicleNo(){
+
+        cout << "water vehc number is not defined" << endl;
+    }
+
+
+};
+
+class Amphibian : public landVehicle, public waterVehicle{
+
+};
+
+int main(){
+
+    Amphibian d;
+    d.landInfo();
+    d.waterInfo();
+    d.chassis = 5;
+
+
+    cout << "number of vehicle is: " << d.chassis << endl;
+    d.landVehicle :: vehicleNo();
+
+    return 0;
+}
